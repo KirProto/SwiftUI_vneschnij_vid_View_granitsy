f5ccf58952d5cf39794b17b3ca40fe6764a07c93
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 05:35:09 +0500

    Initial Commit

---
+++ Yan_SwiftUI_vneschnij_vid_View_granitsy/Yan_SwiftUI_vneschnij_vid_View_granitsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_vneschnij_vid_View_granitsyApp.swift
+//  Yan_SwiftUI_vneschnij_vid_View_granitsy
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_vneschnij_vid_View_granitsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
